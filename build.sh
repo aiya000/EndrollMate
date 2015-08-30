556bef35d2dafca7f6088c36e61e1a2436df8c7d
aiya000 <aiya000.develop@gmail.com>
2015-08-30 11:25:12 +0900

    Created basement

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+tsc --out js/index.js ts/index.ts
---
+++ ts/index.ts
@@ -0,0 +1,9 @@
+/// <reference path="./typings/angularjs/angular.d.ts"/>
+/// <reference path="./typings/jquery/jquery.d.ts"/>
+class BackgroundImageController {
+	constructor() {
+	}
+}
+
+let appEndrollMate = angular.module("appEndrollMate", []);
+appEndrollMate.controller("BackgroundImageController", [BackgroundImageController]);
