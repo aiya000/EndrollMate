e8c109c51c8054793dc4083fd131186fbdf00208
aiya000 <aiya000.develop@gmail.com>
2015-08-31 21:44:29 +0900

    Introduced ng-file-upload

--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,19 @@
 /// <reference path="./typings/angularjs/angular.d.ts"/>
 /// <reference path="./typings/jquery/jquery.d.ts"/>
+/// <reference path="./typings/ng-file-upload/ng-file-upload.d.ts"/>
 class BackgroundImageController {
-	constructor() {
+	public upload:  ng.angularFileUpload.IFileUploadConfig;
+	public bgImage: any;
+
+	/**
+	 * @param このクラスで使用するng.IHttpServiceオブジェクトを設定します
+	 */
+	constructor($upload: ng.angularFileUpload.IFileUploadConfig) {
+		this.upload = $upload;
+	}
+
+	public fileSelect($files: FileList) : void {
+		console.log("foo");
 	}
 }
 
