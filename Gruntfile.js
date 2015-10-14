618260c5ac72eab8ce6a1724777fa960e44e1d57
aiya000 <aiya000.develop@gmail.com>
2015-10-14 14:09:15 +0900

    Introduced grunt

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-typescript');
+	grunt.loadNpmTasks('grunt-babel');
+	grunt.initConfig({
+		typescript : {
+			main : {
+				src : [
+					"ts/index.ts",
+					"ts/MainController.ts",
+					"ts/MainScope.ts",
+					"ts/Maybe.ts",
+					"ts/InvalidOperationError.ts",
+					"ts/Util.ts"
+				],
+				dest : "ts/es6/index.js",
+				options : {
+					target : "es6"
+				}
+			}
+		},
+		babel : {
+			dist : {
+				files : {
+					"js/index.js" : "ts/es6/index.js"
+				}
+			}
+		}
+	});
+	grunt.registerTask("default", ["typescript", "babel"]);
+};
